22cd576c454b78084b9f5486c2416ea7652aec22
ThomasBfrd <115217180+ThomasBfrd@users.noreply.github.com>
2023-02-20 12:56:12 +0100

    Add files via upload

--- videos.js
+++ videos.js
@@ -15,20 +15,20 @@
 
 
 
-document.querySelectorAll('.video-container video').forEach(vid => {
-  vid.onclick = () => {
-    document.querySelector('.popup-video').style.display = "block";
-    document.querySelector('.popup-video video').src = vid.getAttribute('src');
-    document.querySelector('.popup-video video').load()
-  }
-});
-
-document.querySelector('.popup-video span').onclick = () => {
-  document.querySelector('.popup-video').style.display = 'none';
-  document.querySelector('.popup-video video').pause()
-  document.querySelector('.popup-video video').currentTime = 0
+  document.querySelectorAll('.video-container video').forEach(vid => {
+    vid.onclick = () => {
+      document.querySelector('.popup-video').style.display = "block";
+      document.querySelector('.popup-video video').src = vid.getAttribute('src');
+      document.querySelector('.popup-video video').load()
+    }
+  });
   
-}
+  document.querySelector('.popup-video span').onclick = () => {
+    document.querySelector('.popup-video').style.display = 'none';
+    document.querySelector('.popup-video video').pause()
+    document.querySelector('.popup-video video').currentTime = 0
+    
+  }
 
 
 document.querySelectorAll('.video-containerVertical video').forEach(vid => {
