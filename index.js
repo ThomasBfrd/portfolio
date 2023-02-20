5178b30c4275d465687d1a0b9578ceee39012a98
ThomasBfrd <115217180+ThomasBfrd@users.noreply.github.com>
2023-02-20 19:26:51 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -14,3 +14,19 @@ textHover.addEventListener('mouseout', function() {
   prenom.style.transform = "";
   nom.style.transform = "";
 })
+
+const txtAnim = document.querySelector('.txtAnim');
+
+new Typewriter(txtAnim, {
+  loop: true,
+  deleteSpeed: 10
+})
+.changeDelay(30)
+.typeString('Thomas Bouffard')
+.pauseFor(2000)
+.deleteChars(15)
+.pauseFor(1000)
+.typeString('<strong>Développeur Web</strong>')
+.pauseFor(2000)
+.deleteChars(15)
+.start();
