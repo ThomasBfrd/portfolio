b29f5babd33010f06e8d474a334beec40301349d
ThomasBfrd <thomas.bfrd@gmail.com>
2026-01-14 17:39:04 +0100

    feat: ajout des pages

---
+++ src/views/Experiences.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Activity from '@/shared/components/activity.vue';
+import { EXPERIENCES } from '@/shared/const/experiences';
+</script>
+
+<template>
+    <div>
+        <h2 class="text-2xl font-bold mb-6">EXPERIENCES</h2>
+        <Activity :activities="EXPERIENCES" />
+    </div>
+</template>
