61a24b7e7899d8bf6f5fe6d9dcccf582e9070f03
ThomasBfrd <thomas.bfrd@gmail.com>
2026-01-15 15:39:09 +0100

    feat: découpage de la vue a propos

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-import { MENU_ITEMS } from './shared/const/menu-items';
 import { PROFILE } from './shared/const/profile';
-import ArrowRight from "./assets/svg/arrow-right.svg?component";
+import Aside from './shared/components/aside.vue';
 
 const sidebarOpen = ref(false);
+const path = ref("");
+const setSidebarOpened = (open: boolean) => sidebarOpen.value = open;
+const setRouterPath = (newPath: string) => path.value = newPath;
 </script>
 
 <template>
@@ -17,51 +19,9 @@ const sidebarOpen = ref(false);
         </button>
       </div>
 
+      <RouterLink :to="path"></RouterLink>
       <div class="grid sm:grid-cols-[280px_1fr]">
-
-        <aside class="bg-zinc-900 border-r border-zinc-800 p-6 flex flex-col items-center justify-center gap-6
-            sm:flex-col sm:block sm:relative sm:h-auto sm:translate-x-0" :class="{
-              'fixed z-20 top-0 left-0 h-full w-[280px] transform transition-transform duration-300': true,
-              '-translate-x-full': !sidebarOpen,
-              'translate-x-0': sidebarOpen
-            }">
-          <button class="self-end sm:hidden mb-4 p-2 border border-zinc-600 rounded cursor-pointer"
-            @click="sidebarOpen = false">
-            ✕
-          </button>
-
-          <div class="rounded-full h-[120px] w-[120px] overflow-hidden mx-auto">
-            <img class="object-cover h-full w-full" src="/public/profile.jpg" alt="profile-picture">
-          </div>
-
-          <p class="text-zinc-300 text-center">@thomasbfrd</p>
-
-          <div class="flex flex-row gap-4">
-            <div class="langages"></div>
-            <div class="light-dark"></div>
-          </div>
-
-          <div class="w-full">
-            <ul class="flex flex-col gap-4 mt-6">
-              <li v-for="item in MENU_ITEMS" :key="item.label">
-                <RouterLink :to="item.route" @click="sidebarOpen = !sidebarOpen"
-                  class="cursor-pointer flex justify-between px-4 items-center p-2 hover:text-white border border-zinc-700 rounded-[8px] hover:bg-zinc-800 transition-all duration-200 group">
-                  <component :is="item.icon"
-                    class="h-5 w-5 fill-zinc-300 group-hover:fill-white group-hover:-rotate-15 transition-all duration-200" />
-                  <span>{{ item.label }}</span>
-                  <ArrowRight class="h-5 w-5 fill-zinc-300 group-hover:fill-white transition-all duration-200" />
-                </RouterLink>
-              </li>
-            </ul>
-          </div>
-          <div class="mt-8 flex justify-center mx-auto">
-            <svg height="230px" width="230px" viewBox="0 0 230 230">
-              <image
-                xlink:href="https://github-readme-stats-fast.vercel.app/api/top-langs/?username=thomasBfrd&layout=compact&theme=github_dark"
-                width="230" height="230" />
-            </svg>
-          </div>
-        </aside>
+        <Aside :sidebar-open="sidebarOpen" :set-sidebar-open="setSidebarOpened" :set-router-path="setRouterPath" />
         <div class="min-h-screen max-w-screen w-full p-8 border-b border-zinc-800">
           <main class="flex flex-col">
             <RouterView v-slot="{ Component }">
---
+++ src/shared/components/about.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { SOCIALS } from '../const/socials';
+
+defineProps<{
+    isCentered: boolean;
+}>();
+
+</script>
+
+<template>
+    <div>
+        <h2 class="text-2xl font-bold mb-4">Contact</h2>
+        <div class="flex flex-col justify-start items-center gap-4 mb-4"
+            :class="isCentered ? 'items-center' : 'items-start'">
+            <p>thomas.bfrd@gmail.com</p>
+            <ul class="flex gap-8">
+                <li v-for="social in SOCIALS" :key="social.platform">
+                    <a :href="social.url" target="_blank" class="inline-block">
+                        <component :is="social.icon"
+                            class="h-12 w-12 fill-zinc-300 hover:fill-white transition-all duration-300 hover:scale-110" />
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
