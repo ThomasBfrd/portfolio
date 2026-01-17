2f8a3278123b55d74c7cf35a801c2a43fd1bc57a
ThomasBfrd <thomas.bfrd@gmail.com>
2026-01-17 17:30:04 +0100

    feat: ajout de détails dans les différents projets

--- src/shared/interfaces/activity.interface.ts
+++ src/shared/interfaces/activity.interface.ts
@@ -3,7 +3,9 @@ export interface Activity {
     image: string;
     screen: string;
     company: string;
+    website: string;
     period: string;
     tools: string[];
     description: string[];
+    toDo: string[];
 }
